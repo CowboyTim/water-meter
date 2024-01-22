15ece6c308362ab31f628e20ddd47bcd225c0d46
Tim Aerts <aardbeiplantje@gmail.com>
2024-01-22 13:10:15 +0100

    verbose/debug/log improvements

--- water-meter/water-meter.ino
+++ water-meter/water-meter.ino
@@ -13,6 +13,13 @@
 #define LED           2
 #define QRD1114_PIN   0
 
+#ifndef VERBOSE
+#define VERBOSE
+#endif
+#ifndef DEBUG
+#define DEBUG
+#endif
+
 /* NTP server to use, can be configured later on via AT commands */
 #ifndef DEFAULT_NTP_SERVER
 #define DEFAULT_NTP_SERVER "at.pool.ntp.org"
@@ -29,7 +36,8 @@
 char atscbu[128] = {""};
 SerialCommands ATSc(&Serial, atscbu, sizeof(atscbu), "\r\n", "\r\n");
 
-#define CFGVERSION 1
+#define CFGVERSION 0x01
+#define CFGINIT    0x72
 
 /* main config */
 struct wm_cfg {
@@ -40,6 +48,9 @@ struct wm_cfg {
   char ntp_host[64]  = {0};   // max hostname + 1
   unsigned int cnt   = 0;
   unsigned short wifi_timeout = 0;
+  uint8_t do_debug   = 0;
+  uint8_t do_verbose = 0;
+  uint8_t do_log     = 0;
 };
 typedef wm_cfg wm_cfg_t;
 wm_cfg_t cfg = {0};
@@ -50,9 +61,6 @@ uint8_t logged_wifi_status = 0;
 unsigned int last_cnt      = 0;
 unsigned long last_chg     = 0;
 unsigned int v = 0, last_v = 0;
-#ifdef DEBUG
-uint8_t do_debug           = 1;
-#endif
 
 char* at_cmd_check(const char *cmd, const char *at_cmd, unsigned short at_len){
   unsigned short l = strlen(cmd); /* AT+<cmd>=, or AT, or AT+<cmd>? */
@@ -143,12 +151,38 @@ void at_cmd_handler(SerialCommands* s, const char* atcmdline){
     s->GetSerial()->println(cfg.cnt);
   #ifdef DEBUG
   } else if(p = at_cmd_check("AT+DEBUG=1", atcmdline, cmd_len)){
-    do_debug = 1;
+    cfg.do_debug = 1;
+    EEPROM.put(0, cfg);
+    EEPROM.commit();
   } else if(p = at_cmd_check("AT+DEBUG=0", atcmdline, cmd_len)){
-    do_debug = 0;
+    cfg.do_debug = 0;
+    EEPROM.put(0, cfg);
+    EEPROM.commit();
   } else if(p = at_cmd_check("AT+DEBUG?", atcmdline, cmd_len)){
-    s->GetSerial()->println(do_debug);
+    s->GetSerial()->println(cfg.do_debug);
   #endif
+  #ifdef VERBOSE
+  } else if(p = at_cmd_check("AT+VERBOSE=1", atcmdline, cmd_len)){
+    cfg.do_verbose = 1;
+    EEPROM.put(0, cfg);
+    EEPROM.commit();
+  } else if(p = at_cmd_check("AT+VERBOSE=0", atcmdline, cmd_len)){
+    cfg.do_verbose = 0;
+    EEPROM.put(0, cfg);
+    EEPROM.commit();
+  } else if(p = at_cmd_check("AT+VERBOSE?", atcmdline, cmd_len)){
+    s->GetSerial()->println(cfg.do_verbose);
+  #endif
+  } else if(p = at_cmd_check("AT+LOG_CNT=1", atcmdline, cmd_len)){
+    cfg.do_log = 1;
+    EEPROM.put(0, cfg);
+    EEPROM.commit();
+  } else if(p = at_cmd_check("AT+LOG_CNT=0", atcmdline, cmd_len)){
+    cfg.do_log = 0;
+    EEPROM.put(0, cfg);
+    EEPROM.commit();
+  } else if(p = at_cmd_check("AT+LOG_CNT?", atcmdline, cmd_len)){
+    s->GetSerial()->println(cfg.do_log);
   } else if(p = at_cmd_check("AT+ERASE", atcmdline, cmd_len)){
     memset(&cfg, 0, sizeof(cfg));
     EEPROM.put(0, cfg);
@@ -169,23 +203,26 @@ void setup() {
   EEPROM.begin(sizeof(cfg));
   EEPROM.get(0, cfg);
   // was (or needs) initialized?
-  if(cfg.initialized != 72 || cfg.version != CFGVERSION){
+  if(cfg.initialized != CFGINIT || cfg.version != CFGVERSION){
     // clear
     memset(&cfg, 0, sizeof(cfg));
     // reinit
-    cfg.initialized = 72;
+    cfg.initialized = CFGINIT;
     cfg.version     = CFGVERSION;
+    cfg.do_log      = 1;
     strcpy((char *)&cfg.ntp_host, (char *)DEFAULT_NTP_SERVER);
     // write
     EEPROM.put(0, cfg);
     EEPROM.commit();
   }
   #ifdef VERBOSE
-  if(strlen(cfg.wifi_ssid) && strlen(cfg.wifi_pass)){
-    Serial.print(F("will connect via WiFi to "));
-    Serial.println(cfg.wifi_ssid);
-  } else {
-    Serial.print(F("will not connect via WiFi, no ssid/pass"));
+  if(cfg.do_verbose){
+    if(strlen(cfg.wifi_ssid) && strlen(cfg.wifi_pass)){
+      Serial.print(F("will connect via WiFi to "));
+      Serial.println(cfg.wifi_ssid);
+    } else {
+      Serial.print(F("will not connect via WiFi, no ssid/pass"));
+    }
   }
   #endif
 
@@ -201,9 +238,11 @@ void setup() {
   setup_wifi();
 
   #ifdef VERBOSE
-  if(strlen(cfg.ntp_host) && strlen(cfg.wifi_ssid) && strlen(cfg.wifi_pass)){
-    Serial.print(F("will sync with ntp, wifi ssid/pass ok: "));
-    Serial.println(cfg.ntp_host);
+  if(cfg.do_verbose){
+    if(strlen(cfg.ntp_host) && strlen(cfg.wifi_ssid) && strlen(cfg.wifi_pass)){
+      Serial.print(F("will sync with ntp, wifi ssid/pass ok: "));
+      Serial.println(cfg.ntp_host);
+    }
   }
   #endif
   // setup NTP sync to RTC
@@ -214,11 +253,13 @@ void setup() {
   time(&t);
   localtime_r(&t, &gm_new_tm);
   #ifdef VERBOSE
-  Serial.print(F("now: "));
-  Serial.println(t);
-  char d_outstr[100];
-  strftime(d_outstr, 100, "current time: %A, %B %d %Y %H:%M:%S (%s)", &gm_new_tm);
-  Serial.println(d_outstr);
+  if(cfg.do_verbose){
+    Serial.print(F("now: "));
+    Serial.println(t);
+    char d_outstr[100];
+    strftime(d_outstr, 100, "current time: %A, %B %d %Y %H:%M:%S (%s)", &gm_new_tm);
+    Serial.println(d_outstr);
+  }
   #endif
 
   // led to show status
@@ -236,7 +277,7 @@ void loop() {
   v = digitalRead(QRD1114_PIN);
   if(v != last_v){
     #ifdef DEBUG
-    if(do_debug){
+    if(cfg.do_debug){
       Serial.print(F("V: "));
       Serial.print(v);
       Serial.print(F(", LAST_V: "));
@@ -252,10 +293,10 @@ void loop() {
   if(last_cnt != cfg.cnt){
     digitalWrite(LED, HIGH);
     digitalWrite(LED, LOW);
-    #ifdef DEBUG
-    if(do_debug)
+    if(cfg.do_log){
+      Serial.print("CNT,");
       Serial.println(cfg.cnt);
-    #endif
+    }
     last_cnt = cfg.cnt;
     last_chg = millis();
   } else {
@@ -267,8 +308,10 @@ void loop() {
   if(WiFi.status() == WL_CONNECTED){
     if(!logged_wifi_status){
       #ifdef VERBOSE
-      Serial.print(F("WiFi connected: "));
-      Serial.println(WiFi.localIP());
+      if(cfg.do_verbose){
+        Serial.print(F("WiFi connected: "));
+        Serial.println(WiFi.localIP());
+      }
       #endif
       logged_wifi_status = 1;
     }
@@ -284,8 +327,10 @@ void setup_wifi(){
 
   // connect to Wi-Fi
   #ifdef VERBOSE
-  Serial.print(F("Connecting to "));
-  Serial.println(cfg.wifi_ssid);
+  if(cfg.do_verbose){
+    Serial.print(F("Connecting to "));
+    Serial.println(cfg.wifi_ssid);
+  }
   #endif
   WiFi.persistent(false);
   WiFi.begin(cfg.wifi_ssid, cfg.wifi_pass);
